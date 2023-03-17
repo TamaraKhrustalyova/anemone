eddd570d7ed786e6cc3a07b45265e654b776ec62
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-03-17 16:50:56 +0200

    update_header

---
+++ js/slick.js
@@ -0,0 +1,26 @@
+$('.responsive').slick({
+    // dots: true,
+    prevArrow: '<button type="button" class="my-slick-prev"><svg class="slick_arrow"><use href="/combat-geese/assets/sprite-435b49d1.svg#icon-arrow"></use></svg></button>',
+    nextArrow: '<button type="button" class="my-slick-next"><svg class="slick_arrow"><use href="/combat-geese/assets/sprite-435b49d1.svg#icon-arrow"></use></svg></button>',
+    infinite: true,
+    speed: 300,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    slidesToShow: 3,
+    variableWidth: true, 
+  
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
